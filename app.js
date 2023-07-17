3bbd65b633db032d3e81090d1a6d2618095a7aea
MOHAN RAJ S <131248839+Mohanraj-11@users.noreply.github.com>
2023-07-17 15:57:36 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,20 +1,43 @@
-function talk() {
-    var know = {
-        "Who are you": "Hello, S.A.Engineering college ",
-        "How are you": "Good :)",
-        "What can i do for you": "do you how many courses in our college>>Thank Me Later",
-        "courses:":
-            "my college have an 6 UG courses and 3 PG courses ",
-        ok: "Thank You So Much ",
-        Bye: "Okay! Will meet soon..",
-    };
-    var user = document.getElementById("userBox").value;
-    document.getElementById("chatLog").innerHTML = user + "<br>";
-    if (user in know) {
-        document.getElementById("chatLog").innerHTML = know[user] + "<br>";
-    } else {
-        document.getElementById("chatLog").innerHTML =
-            "Sorry,I didn't understand <br>";
-    }
-}
+document.addEventListener('DOMContentLoaded', function() {
+    var input = document.getElementById('user-input');
+    var log = document.getElementById('chat-log');
+    var button = document.getElementById('send-button');
 
+    button.addEventListener('click', function() {
+        var userMessage = input.value;
+        if (userMessage !== '') {
+            appendMessage('You: ' + userMessage);
+            var botMessage = getBotResponse(userMessage);
+            appendMessage('Bot: ' + botMessage);
+            input.value = '';
+        }
+    });
+
+    function appendMessage(message) {
+        var messageElement = document.createElement('p');
+        messageElement.textContent = message;
+        log.appendChild(messageElement);
+        log.scrollTop = log.scrollHeight;
+    }
+
+    function getBotResponse(message) {
+        var response;
+        
+        // Checking for different user inputs and generating appropriate bot responses
+        if (message.toLowerCase().includes('hello') || message.toLowerCase().includes('hi')) {
+            response = 'Hello! How can I assist you?';
+        } else if (message.toLowerCase().includes('courses') || message.toLowerCase().includes('programs')) {
+            response = 'We offer a wide range of programs including Computer Science, Business Administration, and Psychology. Which program are you interested in?';
+        } else if (message.toLowerCase().includes('admission') || message.toLowerCase().includes('apply')) {
+            response = 'To apply for admission, please visit our college website and follow the instructions in the admissions section.';
+        } else if (message.toLowerCase().includes('scholarships') || message.toLowerCase().includes('financial aid')) {
+            response = 'We have various scholarships and financial aid options available. You can find more information on our college website under the financial aid section.';
+        } else if (message.toLowerCase().includes('contact') || message.toLowerCase().includes('information')) {
+            response = 'You can contact our college office at +1-123-456-7890 or send an email to info@college.com.';
+        } else {
+            response = 'I'm sorry, but I don't have information about that. Is there anything else I can assist you with?';
+        }
+
+        return response;
+    }
+});
